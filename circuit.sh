c78a9310ea971b9f6a0916f0912449d43f6d5b05
kylestech95 <kylestech95@outlook.com>
2023-12-30 08:58:16 -0500

    feat: buy tickets for friends/family

--- circuit.sh
+++ circuit.sh
@@ -3,87 +3,146 @@ PSQL="psql -U postgres -d circuit --tuples-only -c"
 echo -e "\n ~~~~2024 Cirtcuit Party Registration~~~~ \n"
 
 # clear tables & restart primary key sequence to 1
-# echo $($PSQL "truncate tickets,rsvp,attendees;alter sequence tickets_ticket_id_seq restart with 1;alter sequence rsvp_rsvp_id_seq restart with 1;alter sequence attendees_attendee_id_seq restart with 1;")
+# echo $($PSQL "truncate rsvp,attendees;alter sequence rsvp_rsvp_id_seq restart with 1;alter sequence attendees_attendee_id_seq restart with 1;")
 
+
+RSVP_INFO(){
+    # rsvp information
+    echo -e "\n~~~~ RSVP information center ~~~~"
+    if [[ $1 ]]
+        then echo -e "\n$1"
+    fi
+    ATTENDEE_ID=$($PSQL "select attendee_id from attendees where phone='$ATTENDEE_PHONE'")
+    NAME_RETRIEVED=$($PSQL "select name from attendees where phone='$ATTENDEE_PHONE'")
+
+
+    echo $NAME_RETRIEVED
+    # obtain customer price from the ticket ID they selected.
+    PRICE=$($PSQL "select price from tickets where ticket_id=$TICKET_SELECTED")
+    echo -e "\nYou must pay me $PRICE."
+    CURRENT_CHARGE=$($PSQL "select price from tickets where ticket_id = $TICKET_SELECTED")
+    # read payment
+    read CUSTOMER_PAYMENT
+    # if payment is insufficient 
+    if [[ "$(echo "$CUSTOMER_PAYMENT < $PRICE" | bc)" = 1 ]]
+        then
+        # echo not enough
+        echo -e "\nInsifficient Amount."
+        #delete customer
+        DROP_CUSTOMER=$($PSQL "delete from attendees where attendee_id = '$ATTENDEE_ID'")
+        MENU "$(echo "$NAME_RETRIEVED" | sed -E 's/^\s+|\s+$//') has been dropped from attendees table."
+    else
+        # if payment is over
+        if [[ "$(echo "$CUSTOMER_PAYMENT > $PRICE" | bc)" = 1 ]]
+            CHANGE_BACK=$(echo $CUSTOMER_PAYMENT-$PRICE | bc)
+            then            
+            #give change back
+            echo -e "\n$(echo "$NAME_RETRIEVED" | sed -E 's/^\s+|\s+$//'), you overpayed, so your change back is: \$$CHANGE_BACK."
+            sleep .5
+            #insert RSVP
+            INSERT_RSVP=$($PSQL "insert into rsvp(ticket_id,attendee_id,customer_payment,customer_change) values($TICKET_SELECTED,$ATTENDEE_ID,'$CUSTOMER_PAYMENT','$CHANGE_BACK')")
+            echo -e "\nRSVP inserted for $(echo "$NAME_RETRIEVED" | sed -E 's/^\s+|\s+$//')."
+        fi
+    fi
+}
 
 
 CUSTOMER_INFO(){
     TICKET_SELECTED=$1
     TICKET_TYPE=$($PSQL "select type from tickets where ticket_id=$TICKET_SELECTED")
-    # phone number
-    echo -e "\nWhat is your phone number?"
-    # read phone number
-    read ATTENDEE_PHONE
-    NAME_RETRIEVED=$($PSQL "select name from attendees where phone='$ATTENDEE_PHONE'")
-    # if phone number is not matched with a customer by ID
-    if [[ -z $NAME_RETRIEVED ]]
-        # How old are you ? 
-        then
-        echo -e "\nHow old are you?"
-        read ATTENDEE_AGE
-            # if age is under 21
-            if [[ $ATTENDEE_AGE -lt 21 ]]
-                then
-                # send to MENU "\nYou are underage."
-                LIMIT=21
-                YEARS_UNTUL_21=$(echo `expr $LIMIT - $ATTENDEE_AGE`)
-                echo -e "\nYou are underage by approximately $YEARS_UNTUL_21 years."
-                # if age is 21 and over
-                else
-                # what is your name?
-                echo -e "\nWhat is your name?"
-                read ATTENDEE_NAME
-                #insert customer information
-                INSERT_ATTENDEE=$($PSQL "insert into attendees(name,age,phone) values('$ATTENDEE_NAME',$ATTENDEE_AGE,'$ATTENDEE_PHONE')")
-                MENU "\n$ATTENDEE_NAME, your rsvp is setup for$TICKET_TYPE.\nSee you there!"
-            fi
-    else
-    # if phone number is matched with customer by ID
-        echo -e "\n$(echo $NAME_RETRIEVED | sed 's/^\s+//'), welcome back."
-        sleep .5
-        # would you like to purchase for family/friends? (The user is simply buying a ticket for someone else.)
-        echo -e "\nWould you like to purchase for a friend/family member? [y|n](case-insensivite)"
-        sleep .5
-        read OPTION
-            # no option makes sense
-            if [[ ! $OPTION =~ [y|Y|n|N] ]]
-                then
-                MENU
-            # if yes
-            else
-                if [[ $OPTION =~ [y|Y] ]]
-                    then
-                    # list tickets options
-                    echo -e "\nWelcome to purchase"
-                    sleep .5
-                    echo "$AVAILABLE_TICKETS" | while read TICKET_ID BAR TYPE BAR PRICE BAR DESCRIPTION
-                    do
-                    echo -e "\n$TICKET_ID) Ticket Type: $TYPE ($DESCRIPTION) - Price: $PRICE"
-                    done
-                    read TICKET_SELECTED
-                    TICKET_MATCH=$($PSQL "select ticket_id from tickets where ticket_id='$TICKET_SELECTED'")
-                    # their phone number
-                    # read phone number
-                    # if phone number is not matched with a customer by ID
-                        # How old are they ? 
-                            # if age is under 21
-                                # send to MENU "\nThey are underage."
-                            # if age is 21 and over
-                                # what is their name?
-                                #insert customer information
-                else
-                    # if no
-                    # send user rsvp information.
-                    echo -e "\n ~~~~RSVP INFORMATION ~~~~\n"
-                    sleep .5
-                    MENU "\n$(echo $NAME_RETRIEVED | sed 's/^\s+//'), your rsvp is setup for$TICKET_TYPE.\nSee you there!"
-                    # MENU
-                fi
-            
-            fi
-            
 
-            
+    if [[ $2 ]]
+    then
+    ATTENDEE_PHONE=$2
+    NAME_RETRIEVED=$($PSQL "select name from attendees where phone='$ATTENDEE_PHONE'")
+        # if phone number is not matched with a customer by ID
+        if [[ -z $NAME_RETRIEVED ]]
+            # How old are you ? 
+            then
+            echo -e "\nHow old are you?"
+            read ATTENDEE_AGE
+                # if age is under 21
+                if [[ $ATTENDEE_AGE -lt 21 ]]
+                    then
+                    # send to MENU "\nYou are underage."
+                    LIMIT=21
+                    YEARS_UNTUL_21=$(echo `expr $LIMIT - $ATTENDEE_AGE`)
+                    # exit the program
+                    echo -e "\nYou are underage by approximately $YEARS_UNTUL_21 years."
+
+                    # if age is 21 and over
+                    else
+                    # what is your name?
+                    echo -e "\nWhat is your name?"
+                    read ATTENDEE_NAME
+                    # insert customer information
+                    sleep 1
+                    INSERT_ATTENDEE=$($PSQL "insert into attendees(name,age,phone) values('$ATTENDEE_NAME',$ATTENDEE_AGE,'$ATTENDEE_PHONE')")
+                    RSVP_INFO "\n$ATTENDEE_NAME, your rsvp is setup for$TICKET_TYPE.\nSee you there!"
+                fi
+        else
+                    # if phone number is matched with customer by ID
+                    echo -e "\n~~~~ $(echo $NAME_RETRIEVED | sed 's/^\s+//'), welcome back ~~~~\n"
+                    sleep .5
+                    # menu option for returnee
+                    echo -e "\nWhat do you want to do?"
+                    sleep .5
+                    echo -e "\n1. Purchase Tickets for friends/family\n2. Donate\n3. Exit"
+                    read MENU_SELECTION
+
+                    case $MENU_SELECTION in 
+                    1) PURCHASE_MENU ;;
+                    2) DONATE_MENU ;;
+                    3) EXIT ;;
+                    esac
+        fi
+    else
+    # phone numbers
+        echo -e "\nWhat is your phone number?"
+        # read phone number
+        read ATTENDEE_PHONE
+        NAME_RETRIEVED=$($PSQL "select name from attendees where phone='$ATTENDEE_PHONE'")
+        # if phone number is not matched with a customer by ID
+        if [[ -z $NAME_RETRIEVED ]]
+            # How old are you ? 
+            then
+            echo -e "\nHow old are you?"
+            read ATTENDEE_AGE
+                # if age is under 21
+                if [[ $ATTENDEE_AGE -lt 21 ]]
+                    then
+                    # send to MENU "\nYou are underage."
+                    LIMIT=21
+                    YEARS_UNTUL_21=$(echo `expr $LIMIT - $ATTENDEE_AGE`)
+                    # exit the program
+                    echo -e "\nYou are underage by approximately $YEARS_UNTUL_21 years."
+
+                    # if age is 21 and over
+                    else
+                    # what is your name?
+                    echo -e "\nWhat is your name?"
+                    read ATTENDEE_NAME
+                    # insert customer information
+                    sleep 1
+                    INSERT_ATTENDEE=$($PSQL "insert into attendees(name,age,phone) values('$ATTENDEE_NAME',$ATTENDEE_AGE,'$ATTENDEE_PHONE')")
+                    RSVP_INFO "\n$ATTENDEE_NAME, your rsvp is setup for$TICKET_TYPE.\nSee you there!"
+                fi
+        else
+                    # if phone number is matched with customer by ID
+                    echo -e "\n~~~~ $(echo $NAME_RETRIEVED | sed 's/^\s+//'), welcome back ~~~~\n"
+                    sleep .5
+                    # menu option for returnee
+                    echo -e "\nWhat do you want to do?"
+                    sleep .5
+                    echo -e "\n1. Purchase Tickets for friends/family\n2. Donate\n3. Exit"
+                    read MENU_SELECTION
+
+                    case $MENU_SELECTION in 
+                    1) PURCHASE_MENU ;;
+                    2) DONATE_MENU ;;
+                    3) EXIT ;;
+                    esac
+        fi
     fi
 
 
@@ -94,22 +153,48 @@ MENU(){
     then echo -e "\n$1"
     fi
 
-    echo -e "\n1. Purchase Tickets\n2. Donate\n3. Exit"
-    read MENU_SELECTION
+    # phone numbers
+    echo -e "\nWhat is your phone number?"
+    # read phone number
+    read ATTENDEE_PHONE
+    NAME_RETRIEVED=$($PSQL "select name from attendees where phone='$ATTENDEE_PHONE'")
+    # if phone number is not matched with a customer by ID
+    if [[ $NAME_RETRIEVED ]]
+    then
+    # if phone number is matched with customer by ID
+        echo -e "\n~~~~ $(echo $NAME_RETRIEVED | sed 's/^\s+//'), welcome back ~~~~\n"
+        sleep .5
+        # menu option for returnee
+        echo -e "\nWhat do you want to do?"
+        sleep .5
+        echo -e "\n1. Purchase Tickets for friends/family\n2. Donate\n3. Exit"
+        read MENU_SELECTION
 
-    case $MENU_SELECTION in 
-    1) PURCHASE_MENU ;;
-    2) DONATE_MENU ;;
-    3) EXIT ;;
-    esac
+        case $MENU_SELECTION in 
+        1) PURCHASE_MENU ;;
+        2) DONATE_MENU ;;
+        3) EXIT ;;
+        esac
+    else
+        echo -e "\n1. Purchase Tickets\n2. Donate\n3. Exit"
+        read MENU_SELECTION
+
+        case $MENU_SELECTION in 
+        1) PURCHASE_MENU $ATTENDEE_PHONE ;;
+        2) DONATE_MENU ;;
+        3) EXIT ;;
+        esac  
+    fi
+
+    
 }
 # purchase menu
 PURCHASE_MENU(){
     # purchase variables
-    AVAILABLE_TICKETS=$($PSQL "select ticket_id,type,price,description from tickets where available=true")
-    AVAILABLE_VIP=$($PSQL "select * from tickets where available=true and type='VIP' ")
-    AVAILABLE_GA=$($PSQL "select * from tickets where available=true and type='GA' ")
-
+    AVAILABLE_TICKETS=$($PSQL "select ticket_id,type,price,description from tickets where available=true order by ticket_id asc")
+    AVAILABLE_VIP=$($PSQL "select * from tickets where available=true and type='VIP'")
+    AVAILABLE_GA=$($PSQL "select * from tickets where available=true and type='GA'")
+    ATTENDEE_PHONE=$1
     # if no tickts are available
     if [[ -z $AVAILABLE_TICKETS ]]
     then
@@ -143,12 +228,12 @@ PURCHASE_MENU(){
                 MENU "\n#$TICKET_SELECTED is not a valid option.\nWelcome to menu"
                 else
                     # continue with customer_information
-                    CUSTOMER_INFO $TICKET_SELECTED
+                    CUSTOMER_INFO $TICKET_SELECTED $ATTENDEE_PHONE
             fi
 
             else
                 # continue with customer_information
-                CUSTOMER_INFO $TICKET_SELECTED
+                CUSTOMER_INFO $TICKET_SELECTED $ATTENDEE_PHONE
         fi
         
 
